da6957b42f1ddeeb74eeba6f8a0a890c7ae4c489
wangzibeng <wzb_0107@163.com>
2019-04-26 21:32:07 +0800

    品牌table

--- src/pages/item/MyBrand.vue
+++ src/pages/item/MyBrand.vue
@@ -1,42 +1,63 @@
 <template>
   <div>
-    <v-data-table
-      :headers="headers"
-      :items="brands"
-      :pagination.sync="pagination"
-      :total-items="totalBrands"
-      :loading="loading"
-      class="elevation-1"
-    >
-      <template slot="items" slot-scope="props">
-        <td class="text-xs-center">{{ props.item.id }}</td>
-        <td class="text-xs-center">{{ props.item.name }}</td>
-        <td class="text-xs-center"><img v-if="props.item.image" :src="props.item.image" width="130" height="40"/></td>
-        <td class="text-xs-center">{{ props.item.letter }}</td>
-      </template>
-    </v-data-table>
+    <v-card>
+      <v-card-title>
+        <v-text-field label="请输入关键字" v-model="search" append-icon="search" hide-details></v-text-field>
+        <v-spacer/>
+        <v-btn color="primary">
+          添加
+        </v-btn>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="brands"
+        :pagination.sync="pagination"
+        :total-items="totalBrands"
+        :loading="loading"
+        class="elevation-1"
+      >
+        <template slot="items" slot-scope="props">
+          <td class="text-xs-center">{{ props.item.id }}</td>
+          <td class="text-xs-center">{{ props.item.name }}</td>
+          <td class="text-xs-center"><img v-if="props.item.image" :src="props.item.image" width="130" height="40"/></td>
+          <td class="text-xs-center">{{ props.item.letter }}</td>
+
+          <!--删除 修改按钮-->
+          <td class="text-xs-center">
+            <v-icon small class="mr-2" @click="editItem(props.item)">
+              edit
+            </v-icon>
+            <v-icon small @click="deleteItem(props.item)">
+              delete
+            </v-icon>
+          </td>
+        </template>
+      </v-data-table>
+    </v-card>
   </div>
 </template>
 
 <script>
   export default {
     name: "myBrand",
-    data () {
+    data() {
       return {
         totalBrands: 0, // 总条数
         brands: [], // 当前页品牌数据
         loading: true, // 是否在加载中
         pagination: {}, // 分页信息
+        search: "",
         headers: [ // 头信息
-          {text: 'id', align: 'center', value: 'id'},
-          {text: '名称', align: 'center', value: 'name', sortable: false},
+          {text: 'ID', align: 'center', value: 'id'},
+          {text: '品牌', align: 'center', value: 'name', sortable: false},
           {text: 'LOGO', align: 'center', value: 'image', sortable: false},
           {text: '首字母', align: 'center', value: 'letter'},
+          {text: '操作', align: 'center', value: 'id', sortable: false},
         ]
       }
     },
     methods: {
-      getDataFromServer(){ // 从服务端加载数据的函数
+      getDataFromServer() { // 从服务端加载数据的函数
         // 伪造演示数据
         const brands = [
           {
@@ -47,10 +68,31 @@
             "categories": null
           },
           {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
+            "id": 2033,
+            "name": "飞利浦（PHILIPS）",
+            "image": "http://img12.360buyimg.com/popshop/jfs/t18361/122/1318410299/1870/36fe70c9/5ac43a4dNa44a0ce0.jpg",
+            "letter": "F",
+            "categories": null
+          },
+          {
+            "id": 2034,
+            "name": "华为（HUAWEI）",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t5662/36/8888655583/7806/1c629c01/598033b4Nd6055897.jpg",
+            "letter": "H",
+            "categories": null
+          },
+          {
+            "id": 2036,
+            "name": "酷派（Coolpad）",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2521/347/883897149/3732/91c917ec/5670cf96Ncffa2ae6.jpg",
+            "letter": "K",
+            "categories": null
+          },
+          {
+            "id": 2037,
+            "name": "魅族（MEIZU）",
+            "image": "http://img13.360buyimg.com/popshop/jfs/t3511/131/31887105/4943/48f83fa9/57fdf4b8N6e95624d.jpg",
+            "letter": "M",
             "categories": null
           },
           {
@@ -61,50 +103,36 @@
             "categories": null
           },
           {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
+            "id": 2033,
+            "name": "飞利浦（PHILIPS）",
+            "image": "http://img12.360buyimg.com/popshop/jfs/t18361/122/1318410299/1870/36fe70c9/5ac43a4dNa44a0ce0.jpg",
+            "letter": "F",
             "categories": null
           },
           {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
+            "id": 2034,
+            "name": "华为（HUAWEI）",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t5662/36/8888655583/7806/1c629c01/598033b4Nd6055897.jpg",
+            "letter": "H",
             "categories": null
           },
           {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
+            "id": 2036,
+            "name": "酷派（Coolpad）",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2521/347/883897149/3732/91c917ec/5670cf96Ncffa2ae6.jpg",
+            "letter": "K",
             "categories": null
           },
           {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
-            "categories": null
-          },
-          {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
-            "categories": null
-          },
-          {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
+            "id": 2037,
+            "name": "魅族（MEIZU）",
+            "image": "http://img13.360buyimg.com/popshop/jfs/t3511/131/31887105/4943/48f83fa9/57fdf4b8N6e95624d.jpg",
+            "letter": "M",
             "categories": null
           }
         ];
         // 延迟一段时间，模拟数据请求时间
-        setTimeout(()=>{
+        setTimeout(() => {
           this.brands = brands; // 赋值给品牌数组
           this.totalBrands = brands.length; // 赋值数据总条数
           this.loading = false; // 数据加载完成
@@ -112,7 +140,7 @@
       }
     },
     // 渲染后执行
-    mounted(){
+    mounted() {
       this.getDataFromServer() // 调用数据初始化函数
     }
   }
