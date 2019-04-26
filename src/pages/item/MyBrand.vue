b3857cc9e69095bcb82088a96f68abe4f504e084
wangzibeng <wzb_0107@163.com>
2019-04-26 13:59:48 +0800

    品牌table

---
+++ src/pages/item/MyBrand.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="brands"
+      :pagination.sync="pagination"
+      :total-items="totalBrands"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-center">{{ props.item.id }}</td>
+        <td class="text-xs-center">{{ props.item.name }}</td>
+        <td class="text-xs-center"><img v-if="props.item.image" :src="props.item.image" width="130" height="40"/></td>
+        <td class="text-xs-center">{{ props.item.letter }}</td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "myBrand",
+    data () {
+      return {
+        totalBrands: 0, // 总条数
+        brands: [], // 当前页品牌数据
+        loading: true, // 是否在加载中
+        pagination: {}, // 分页信息
+        headers: [ // 头信息
+          {text: 'id', align: 'center', value: 'id'},
+          {text: '名称', align: 'center', value: 'name', sortable: false},
+          {text: 'LOGO', align: 'center', value: 'image', sortable: false},
+          {text: '首字母', align: 'center', value: 'letter'},
+        ]
+      }
+    },
+    methods: {
+      getDataFromServer(){ // 从服务端加载数据的函数
+        // 伪造演示数据
+        const brands = [
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          },
+          {
+            "id": 2032,
+            "name": "OPPO",
+            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
+            "letter": "O",
+            "categories": null
+          }
+        ];
+        // 延迟一段时间，模拟数据请求时间
+        setTimeout(()=>{
+          this.brands = brands; // 赋值给品牌数组
+          this.totalBrands = brands.length; // 赋值数据总条数
+          this.loading = false; // 数据加载完成
+        }, 1000);
+      }
+    },
+    // 渲染后执行
+    mounted(){
+      this.getDataFromServer() // 调用数据初始化函数
+    }
+  }
+</script>
+
+<!-- scoped:当前样式只作用于当前组件的节点 -->
+<style scoped>
+
+</style>
